5b41acde52fc14efee4b5b0decf2ec33de23b487
Zayceur <zayceur@gmail.com>
2021-01-11 02:50:16 +0100

    Change publish to API

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,2 +1,26 @@
+plugins {
+    `maven-publish`
+}
+
 group = "ch.swisscypher.u14n.api"
 version = "0.1-SNAPSHOT"
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/swisscypher/u14n")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("api") {
+            from(components["java"])
+            // Include any other artifacts here, like javadocs
+        }
+    }
+}
